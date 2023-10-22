689ac0ea27f84d8d69fafd551c6a7a067682a884
KasimRIT <klo7619@rit.edu>
2023-10-22 03:52:11 -0400

    Fixed error with test

--- backend/tests/application.test.js
+++ backend/tests/application.test.js
@@ -39,8 +39,10 @@ describe('application ', () => {
             notes: "Working on creating a job description, verbal offer."
         };
 
-        const application = await createApplication(applicationForm.role, applicationForm.jobType, applicationForm.experienceLevel, applicationForm.company,
+        const {applicationId} = await createApplication(applicationForm.role, applicationForm.jobType, applicationForm.experienceLevel, applicationForm.company,
             applicationForm.location, applicationForm.appliedDate, applicationForm.status, applicationForm.notes)
+        
+        const application = await Application.findById(applicationId)
 
         expect(application.role).toEqual(applicationForm.role)
     });
